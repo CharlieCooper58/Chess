251ccb5680e4f1634a00e48600fd12c26268e8dd
2006572 <2006572@AL-105-05.student.acalanes.k12.ca.us>
2017-03-17 12:37:37 -0700

    AI is thinking...

--- TicTacToe/tictac-final/src/tictac/pkgfinal/AI.java
+++ TicTacToe/tictac-final/src/tictac/pkgfinal/AI.java
@@ -158,6 +158,12 @@ public class AI {
      return coor;   
     }
     public void move(){
+        for(int i = 0; i<board.length; i++){
+                   for(int j = 0; j<board[0].length; j++){
+                       System.out.print(board[i][j]);
+                    }
+                   System.out.println();
+                }
         System.out.println("Computer is thinking...");
         try {
             TimeUnit.SECONDS.sleep(1);
@@ -195,12 +201,6 @@ public class AI {
             }
         }
     public void AIMove(int row, int col){
-        for(int i = 0; i<board.length; i++){
-                   for(int j = 0; j<board[0].length; j++){
-                       System.out.print(board[i][j]);
-                    }
-                   System.out.println();
-                }
         if(board[row][col].equals("+")) {
                board[row][col] = "O";
         }
