3c7cf6fc7f2b2cd7a289e5f53b9406f178d7bd51
CharlieCooper58 <ccooper600@comcast.net>
2017-03-28 13:03:48 -0700

    Update AI.java

--- TicTacToe/tictac-final/src/tictac/pkgfinal/AI.java
+++ TicTacToe/tictac-final/src/tictac/pkgfinal/AI.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tictac.pkgfinal;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
@@ -32,7 +26,7 @@ public class AI {
                }
                 //If the computer has 2 in a row, it will then figure out if the third space is
                 //open.  If it is, then it will return that space's coordinates.
-                   if(hori == 2){
+               if(hori == 2){
                    for(int j = 0; j<b.board[1].length; j++){
                        if(b.board[i][j].equals("+")){
                            coor[0] = i;
@@ -41,32 +35,28 @@ public class AI {
                                
                            }
                        }
-                   //Resets hori so as not to mess over future iterations of the loop.
-                    hori = 0;
-                   
-                   }
+                //Resets hori so as not to mess over future iterations of the loop.
+               hori = 0;
+        }
         //Determines if the computer has 2 in a column.  The code is the same as the other one,
         //except that x and y are inverted so as to check vertically instead of horizontally.
         for(int i = 0; i<b.board[0].length; i++){
-               for(int j = 0; j<b.board[1].length; j++){
+              for(int j = 0; j<b.board[1].length; j++){
                    if(b.board[j][i].equals("O")){
                        vert ++;
                    }
-               }
-                   if(vert == 2){
+              }
+              if(vert == 2){
                    for(int j = 0; j<b.board[1].length; j++){
                        if(b.board[j][i].equals("+")){
                            coor[0] = j;
                            coor[1] = i;
                            vert = 0;
-                           }
-                               
-                           }
-                       }
-                  
-                       vert = 0;
-                   
+                       } 
                    }
+              } 
+              vert = 0;   
+        }
         //Checks the diagonals.  If 2 of the diagonal spots are O's and the third is open,
         //the computer will move there.
         if(b.board[0][0].equals("O")&&b.board[1][1].equals("O")&&b.board[2][2].equals("+")){
@@ -93,7 +83,7 @@ public class AI {
             coor[0] = 2;
             coor[1] = 0;
         }
-     return coor;   
+        return coor;   
     }
     public int[] canHeWin(){
         //This method is basically the same as the above method, except that it checks for X's
@@ -119,28 +109,24 @@ public class AI {
 
                     }
                }
-                       hori = 0;
-                   
-               }
-            
+               hori = 0;
+               }     
         for(int i = 0; i<b.board[0].length; i++){
                for(int j = 0; j<b.board[1].length; j++){
                    if(b.board[j][i].equals("X")){
                        vert ++;
                    }
                }
-                   if(vert == 2){
+               if(vert == 2){
                    for(int j = 0; j<b.board[1].length; j++){
                        if(b.board[j][i].equals("+")){
                            coor[0] = j;
                            coor[1] = i;
                            vert = 0;
-                           }
-                               
-                           }
-                       }
-                       vert = 0;
-                   
+                       }             
+                   }
+               }
+               vert = 0;                 
         }
         if(b.board[0][0].equals("X")&&b.board[1][1].equals("X")&&b.board[2][2].equals("+")){
             coor[0] = 2;
@@ -166,18 +152,9 @@ public class AI {
             coor[0] = 2;
             coor[1] = 0;
         }
-     return coor;   
+        return coor;   
     }
     public void move(){
-        b.printBoard();
-        System.out.println("Computer is thinking...");
-        //Pauses the code for one second to give the illusion that the computer is thinking.  
-        //We've found that it's disconcerting to have the computer immediately move.
-        try {
-            TimeUnit.SECONDS.sleep(1);
-        } catch (InterruptedException ex) {
-            Logger.getLogger(AI.class.getName()).log(Level.SEVERE, null, ex);
-        }
         int[] coor = new int[2];
         coor[0] = -1;
         coor[1] = -1;
@@ -204,9 +181,9 @@ public class AI {
             if(b.board[row][col].equals("+")){
                 AIMove(row, col);
                 moved = true;
-        }
             }
         }
+    }
     //Double-checks that the desired space is available, and if it is, then the AI will move there.
     public void AIMove(int row, int col){
         if(b.board[row][col].equals("+")) {
