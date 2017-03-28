c6c3147226b5f243f9d9e77e9391031363bb4754
CharlieCooper58 <ccooper600@comcast.net>
2017-03-28 13:04:19 -0700

    Update Board.java

--- TicTacToe/tictac-final/src/tictac/pkgfinal/Board.java
+++ TicTacToe/tictac-final/src/tictac/pkgfinal/Board.java
@@ -1,10 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tictac.pkgfinal;
-
 import java.util.Scanner;
 /**
  *
@@ -14,21 +7,17 @@ import java.util.Scanner;
 public class Board {
     public String[][] board = new String[3][3];
     public Board(){
-           for(int i = 0; i<board[0].length; i++){
-                   for(int j = 0; j< board[1].length; j++){
-                      board[i][j] = "+";
-                    }
-                }
+        for(int i = 0; i<board[0].length; i++){
+            for(int j = 0; j< board[1].length; j++){
+                board[i][j] = "+";
+            }
+        }
     }
-    //Prints the board
-    public void printBoard(){
-        System.out.println("  " + 123);
-        for(int i = 0; i<board.length; i++){
-            System.out.print(i+1 + " ");
-                   for(int j = 0; j<board[0].length; j++){
-                       System.out.print(board[i][j]);
-                    }
-                   System.out.println();
-                }
+    public void clearBoard(){
+        for(int i = 0; i<board[0].length; i++){
+            for(int j = 0; j< board[1].length; j++){
+                 board[i][j] = "+";
+            }
+        }
     }
 }
